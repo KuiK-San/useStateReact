790c46958e66b7b666411813779f3a0b1972eeef
root <root@NVPC>
2024-02-07 12:07:49 -0300

    feat: firstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { sculptureList } from "./data"
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Container(){
+    const [index, setIndex] = useState(0)
+    const tamanho = sculptureList.length - 1
+    console.log(sculptureList)
+    function nextPage(){
+        console.log(tamanho)
+        console.log(index)
+
+        if(index >= tamanho){
+            setIndex(0)
+        }else{
+            setIndex(index + 1)
+        }
+    }
+    const data = sculptureList[index]
+    return(
+        <>
+        <button onClick={nextPage}>Próximo</button>
+        <div>
+            {data.name}<br/>
+            {data.description}
+        </div>
+        </>
+    )
 }
-
-export default App;
